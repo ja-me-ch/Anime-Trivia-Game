08c8d52688ce6762d986988c63e3037554bb2b23
ja-me-ch <60724072+ja-me-ch@users.noreply.github.com>
2023-02-25 21:04:42 -0500

    added meta tags AnimeTriviaGame.js

--- components/AnimeTriviaGame.js
+++ components/AnimeTriviaGame.js
@@ -10,11 +10,12 @@ import BottomBar from './Stage/BottomBar';
 import { AnimeTriviaGameContext } from '../contexts/AnimeTriviaGameContext';
 import { display } from '@mui/system';
 import Default from './Dialog/Default';
+import Head from "next/head";
 
-const RootStyle = styled('div')(() => ({
-    display: 'grid',
-    position: 'relative',
-    gridTemplateRows: 'auto 1fr auto',
+const RootStyle = styled("div")(() => ({
+    display: "grid",
+    position: "relative",
+    gridTemplateRows: "auto 1fr auto",
     // width: '100%',
     // border: '1px solid teal',
     // minWidth: '100%',
@@ -23,41 +24,52 @@ const RootStyle = styled('div')(() => ({
     // maxHeight: '100vh',
 }));
 
-const RightSideBar = styled('div')(() => ({
+const RightSideBar = styled("div")(() => ({
     // border: '1px solid yellow'
 }));
 
-const CenterContainer = styled('div')(() => ({
-    flexGrow: '4',
+const CenterContainer = styled("div")(() => ({
+    flexGrow: "4",
     // border: '1px solid green'
 }));
 
 function AnimeTriviaGame() {
     return (
         <QuestionAndAnswerProvider>
+            <Head>
+                <title>Anime Trivia Game</title>
+                <meta property="og:type" content="website" />
+                <meta
+                    property="og:url"
+                    content="https://ja-me-ch.github.io/Anime-Trivia-Game/"
+                />
+                <meta property="og:title" content="Anime Trivia Game" />
+                <meta
+                    property="og:description"
+                    content="An Anime Trivia Game that generates trivia from your profile on AniList.co"
+                />
+            </Head>
             <RootStyle>
                 <TopBar />
 
                 <CenterBar />
 
-                <div style={{
-                    // border: '1px solid red',
-                    display: 'flex',
-                    // minWidth: '100vw',
-                    // width: '99vw',
-                    maxWidth: '100vw',
-                    flexDirection: 'row-reverse',
-                    position: 'fixed',
-                    bottom: '0',
-                    right: '0'
-                }}>
+                <div
+                    style={{
+                        display: "flex",
+                        maxWidth: "100vw",
+                        flexDirection: "row-reverse",
+                        position: "fixed",
+                        bottom: "0",
+                        right: "0",
+                    }}
+                >
                     <BottomBar />
                 </div>
                 <Default />
             </RootStyle>
         </QuestionAndAnswerProvider>
-
-    )
+    );
 }
 
 export default AnimeTriviaGame
